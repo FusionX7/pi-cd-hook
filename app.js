110e7f6d5178c73e438321991fcf9a12c026e1b7
Nguyen Huu Phuoc <phuoc01.nguyenhuu@haravan.com>
2020-06-27 01:26:02 +0700

    fix build

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.post('/payload', function (req, res) {
 	send(`<a href="${req.body.repository.html_url}">${req.body.repository.name}</a>
 	<b>${req.body.pusher.name}</b> just pushed to <b>${req.body.ref}</b>
 	<a href="${req.body.head_commit.url}">${req.body.head_commit.message}</a>`);
-	if(pushedBranch !== 'master' && pushedBranch !== 'staging') return;
+	if(pushedBranch === 'master' || pushedBranch === 'staging')
 	switch (req.body.repository.name) {
 		case 'fusion-web':
 			build(branch,WEB_DIR_SOURCE,copyAssets);
